715b6934c3104b549d019a564ae9283a7fb82762
QuaggWasTaken <forrestfaulkner1@gmail.com>
2020-11-08 15:15:23 -0600

    Office Hours Attempt 1

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
                 model: Comment,
                 attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
                 order: [
-                    [Comment, 'id', 'DESC']
+                    [Comment, 'created_at', 'DESC']
                 ],
                 include: {
                     model: User,
